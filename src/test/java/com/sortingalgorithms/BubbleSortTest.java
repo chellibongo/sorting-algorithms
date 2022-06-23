5ebda6acdcbd7320e55a7ff3397df769d883337e
Chris Ellis <ce8958@gmail.com>
2022-06-23 15:31:18 +0100

    test: added first bubblesort test

--- src/test/java/com/sortingalgorithms/BubbleSortTest.java
+++ src/test/java/com/sortingalgorithms/BubbleSortTest.java
@@ -1,20 +1,29 @@
 package com.sortingalgorithms;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.Test;;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;;
 
 /**
  * Unit test for simple App.
  */
 public class BubbleSortTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
+    BubbleSort bubbleSort;
+
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    @DisplayName("Test sorting when given array of integers")
+    void testSort() {
+        bubbleSort = new BubbleSort();
+        int[] testArray = {2,1};
+        int[] expectedArray = {1,2};
+        bubbleSort.sort(testArray);
+        assertArrayEquals(testArray,expectedArray);
     }
 }
