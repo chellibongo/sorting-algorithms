f0e0bdeb89f674b1dc6d2d747e5558b94705b6f5
Chris Ellis <ce8958@gmail.com>
2022-06-24 13:01:05 +0100

    test: added unit tests for bubble sort

--- src/test/java/com/sortingalgorithms/BubbleSortTest.java
+++ src/test/java/com/sortingalgorithms/BubbleSortTest.java
@@ -4,10 +4,15 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.Random;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;;
+import org.junit.jupiter.api.TestInstance;
+
+import org.apache.commons.lang3.ArrayUtils;
 
 /**
  * Unit test for simple App.
@@ -17,8 +22,8 @@ public class BubbleSortTest
     BubbleSort bubbleSort;
 
 
-    @Test
     @DisplayName("Test sorting when given array of integers")
+    @Test
     void testSort() {
         bubbleSort = new BubbleSort();
         int[] testArray = {2,1};
@@ -26,4 +31,28 @@ public class BubbleSortTest
         bubbleSort.sort(testArray);
         assertArrayEquals(testArray,expectedArray);
     }
+
+    @DisplayName("Test sorting empty array")
+    @Test
+    void testSortEmptyArray() {
+        bubbleSort = new BubbleSort();
+        int[] testArray = {};
+        int[] expectedArray = {};
+        bubbleSort.sort(testArray);
+        assertArrayEquals(expectedArray, testArray);
+    }
+
+    @DisplayName("Test sorting array of 100 elements")
+    @Test
+    void testSortLargeArray() {
+        bubbleSort = new BubbleSort();
+        Random random = new Random();
+        int[] testArray = random.ints(100, 0, 100).toArray();
+
+        bubbleSort.sort(testArray);
+        
+        assertTrue(ArrayUtils.isSorted(testArray));
+    }
+
+
 }
